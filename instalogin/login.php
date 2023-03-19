6a546d8c52b123d0f5d3811197f0938840037350
ratnesh8957 <ratneshd8931@gmail.com>
2023-03-19 16:13:27 +0530

    instagramlogin

---
+++ instalogin/login.php
@@ -0,0 +1,35 @@
+<?php
+$username = filter_input(INPUT_POST,'username');
+$password = filter_input(INPUT_POST,'password');
+
+if(!empty($username)){
+  if(!empty($password)){
+$host = "localhost";
+$dbusername ="root";
+$dbpassword="";
+$dbname="instalogin";
+$conn = new mysqli ($host,$dbusername,$dbpassword,$dbname);
+if(mysqli_connect_error()){
+  die('Connect Error('. mysqli_connect_error().')'.mysqli_connect_error());
+}
+else{
+  $sql ="INSERT INTO welcome.php( username , password) values('$username','password')";
+  if($conn->query($sql)){
+    echo "new record inserted successful";
+  }
+  else{
+    echo"Error:".$sql."<br>".$conn->error;
+  }
+  $conn->close();
+}
+  } 
+else{
+  echo"Password should not match";
+  die();
+}
+}
+else{
+  echo"Username should not be empty";
+}
+die();
+
